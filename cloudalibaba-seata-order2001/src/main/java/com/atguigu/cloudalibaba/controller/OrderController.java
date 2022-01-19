7d6cf331a4e370c321b32e9b69fb5fa34f0696fc
sanggao618 <764324677@qq.com>
2022-01-19 13:25:30 +0800

    commit

--- cloudalibaba-seata-order2001/src/main/java/com/atguigu/cloudalibaba/controller/OrderController.java
+++ cloudalibaba-seata-order2001/src/main/java/com/atguigu/cloudalibaba/controller/OrderController.java
@@ -5,7 +5,6 @@ import com.atguigu.cloudalibaba.domain.Order;
 import com.atguigu.cloudalibaba.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
